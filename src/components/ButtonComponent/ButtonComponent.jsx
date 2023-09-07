619af656a2c0a5cc6916bd627e8116679bec03ba
Federico Cairo <ffedecairo@gmail.com>
2023-09-06 23:28:49 -0300

    primer comit de la segunda pre entrega

---
+++ src/components/ButtonComponent/ButtonComponent.jsx
@@ -0,0 +1,6 @@
+import Button from 'react-bootstrap/Button';
+  const ButtonComponent = ({label, bsButtonType, onClickFunction}) => {
+  return <Button variant={bsButtonType} onClick={onClickFunction}>{label}</Button>;
+};
+
+export default ButtonComponent;
---
+++ src/components/LoaderComponent/LoaderComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { DotPulse } from '@uiball/loaders'
+
+const LoaderComponent = () => {
+    return <div>
+        <DotPulse size={40} speed={1.3} color="black" />
+    </div>
+};
+
+export default LoaderComponent;
